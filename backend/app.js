a1d7d413f8d12b4c9083736c024ec30f8d2deb02
Flavien Ropers <ropersflavien@gmail.com>
2021-02-09 19:52:05 +0100

    Sécurité (module: express-rate-limiter) Peaufinage

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,10 @@ mongoose.connect('mongodb+srv://user:46BaT7@cluster0.kg9bl.mongodb.net/projet_6?
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
 
---
+++ backend/middleware/express-rate-limit-config.js
@@ -0,0 +1,7 @@
+const rateLimit = require("express-rate-limit");
+
+exports.LoginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10// limites à 10 requêtes toutes les 15 minutes
+});
+
